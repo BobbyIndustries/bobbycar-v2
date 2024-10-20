9c5350064ea7c35067a343fddd0745a2350f1d5b
LayerDE <the_gamer@gmx.net>
2024-10-20 23:15:21 +0200

    update

---
+++ main/sw_features/so_bluepad32_handler.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void c_cb_so_blueoad32_handler_onConnectedController(void* Controller);
+extern void c_cb_so_blueoad32_handler_onDisconnectedController(void* Controller);
+extern void c_cb_so_blueoad32_handler_onConnectController(void* Controller);
+extern void c_cb_so_blueoad32_handler_onConnectController(void* Controller);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ main/sw_features/so_bluepad32_handler.hpp
@@ -0,0 +1,8 @@
+#include <Bluepad32.h>
+
+class so_bluepad32_handler{
+    private:
+        ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+    public:
+        so_bluepad32_handler();
+};
